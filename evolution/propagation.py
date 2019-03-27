eb7586c9e52bfe8b5bc92224518d19544e99585f
Federico Roy <38183071+fedroy@users.noreply.github.com>
2019-03-27 14:18:19 +0100

    added skeleton of propagation function

--- evolution/propagation.py
+++ evolution/propagation.py
@@ -1,6 +1,12 @@
-def propagate_gradient(h, u0, ts, grad = False, history = False):
-    """Find the propagator of a system Hamiltonian h(t). The initial basis u0 can be a statem a unitary
-    or a rectangular matrix made of several initial states. The ha
+def sesolve_pwc(H, u0, tlist, grad = False, history = False):
     """
-    us_with_grad = []
-    return us_with_grad
+    Find the propagator of a system Hamiltonian h(t). The initial basis u0  The hamiltonian
+    :param H: :class:'qutip.qobj' System hamiltonian or a list of Drift and Control terms
+    :param u0: can be a state, a unitary or a rectangular matrix made of several initial states.
+    :param tlist: time vector
+    :output: list of unitaries (Qobj) for all times in tlist or just the initial and final time (depending on history setting)
+    """
+    
+    us_of_t = []
+    
+    return us_of_t
