a71a948d4e817adb632fabb4f74b472350caca58
nwittler <s9niwitt@stud.uni-saarland.de>
2019-03-28 12:46:37 +0100

    Started control definitions.

--- goat/goat.py
+++
@@ -1,5 +0,0 @@
-"""This file contains the necessary infrastructure for the GOAT optimal control technique."""
-
-def optimize_controls(p_init):
-    propagate_with_grad()
-    return best_p
--- main/problem.py
+++ main/problem.py
@@ -8,13 +8,13 @@ class System:
     Abstraction of system in terms of the parts that compose it, e.g. two qubits and a cavity.
     (registers, device specs), this is what the experimentalist tells you.
     It constructs models for the system.
-    
+
     Parameters
     ----------
     :param components:
     :param connection:
     :param phys_params:
-    
+
     Methods
     -------
     construct_model(system_parameters, numerical_parameters)
@@ -22,26 +22,26 @@ class System:
     get_parameters()
         Produces a dict of physical system parameters
     """
-    
+
 class Model:
     """
     What the theorist thinks about from the system.
     Class to store information about our system/problem/device. Different models can represent the same system.
-    
+
     Parameters
     ---------
     physical_parameters : dict
         Represents the beta in GOAT language. Contains physical parameters as well as Hilbert space dimensions, bounds
     numerical_parameters : dict
         Hilbert space dimensions of computational and full spaces
-    
+
     Attributes
     ----------
     H: :class:'qutip.qobj' System hamiltonian or a list of Drift and Control terms
     H_tf : empty, constructed when needed
-    system_parameters : 
-    numerical_parameters : 
-    
+    system_parameters :
+    numerical_parameters :
+
     Methods
     -------
     construct_Hamiltonian(system_parameters, numerical_parameters)
@@ -53,28 +53,41 @@ class Model:
         self.system_parameters = system_parameters
         self.numerical_parameters = numerical_parameters
         self.H = self.construct_Hamiltonian(system_parameters, numerical_parameters)
-        self.projector = utils.rect_space(H[0].dims[0], comp_dims) #rect identity for computation 
-        
+        self.projector = utils.rect_space(H[0].dims[0], comp_dims) #rect identity for computation
+
 class Problem:
     """
     Main Class. CLass to specify the problem that we are trying to solve.
-    
+
     Parameters
     ---------
-    goal_func: maybe string/handles
-    gates_to_optimize: as string/handles or abstract objects
-    system: If you want to learn the model for this system.
-    initial_model: (optional) Can be constructed from system or given directly.
-    experiment_backend: Calls the actual physical system (or simulation thereof) that you want to learn and that we will do the calibration on.
-    
+    goal_func:
+        maybe string/handles
+    gates_to_optimize:
+        as string/handles or abstract objects
+    system:
+        If you want to learn the model for this system.
+    initial_model:
+        (optional) Can be constructed from system or given directly.
+    simulation_backend:
+        Provides measurement results from simulated dynamics.
+    experiment_backend:
+        Calls the actual physical system (or simulation thereof) that you want to learn and that we will do the calibration on.
+    optimal_control_frontend:
+        Contains the search algorithm and infrastructure
+
+    Attributes
+    ----------
+    optimal_pulses:
+        Correspond to the gates_to_optimize
+    model_history:
+        Models improved by learning from calibration (Step III)
+
     Methods
     -------
     get_Hamiltonian()
         model.construct_Hamiltonian() Placeholder
     """
-    
 
-    
-    
-    
-    
+    return
+
