018ccb3f1872d616ad8a757331fb6f3d73429bde
Nico <s9niwitt@stud.uni-saarland.de>
2019-05-21 17:31:18 +0200

    Setup adjusted.

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from distutils.core import setup
 setup(
     name='c3po',
     version='0.1dev',
-    packages=['fidelity', 'main', 'utils', ],
+    packages=['c3po', 'c3po/fidelity', 'c3po/main', 'c3po/utils', ],
     long_description=open('README.md').read(),
 )
