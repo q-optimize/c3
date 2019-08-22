3f8bf9995e124e8c084280d3b096b455fa337419
kepack <kevin.pack89@gmail.com>
2019-08-22 17:53:35 +0200

    Added (hacked) version of new signal generation

---
+++ c3po/signals/generator.py
@@ -0,0 +1,261 @@
+import uuid
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+
+
+class Device:
+    """
+    """
+    def __init__(
+            self,
+            name = " ",
+            desc = " ",
+            comment = " ",
+            resolutions = {},
+            ressources = [],
+            ressource_groups = {}
+            ):
+
+        self.name = name
+        self.desc = desc
+        self.comment = comment
+        self.resolutions = resolutions
+        self.ressources = ressources
+        self.ressource_groups = ressource_groups
+
+
+    def calc_slice_num(self, res_key):
+        res = self.resolutions[res_key]
+
+        if self.t_start != None and self.t_end != None and res != None:
+            self.slice_num = (int(np.abs(self.t_start - self.t_end) * res) + 1)
+        else:
+            self.slice_num = None
+
+
+    def create_ts(self, res_key):
+        if self.t_start != None and self.t_end != None and self.slice_num != None:
+            self.ts = np.linspace(self.t_start, self.t_end, self.slice_num)
+        else:
+            self.ts = None
+
+
+
+
+
+class Mixer(Device):
+    """
+    """
+    def __init__(
+            self,
+            t_start = None,
+            t_end = None
+            ):
+
+        self.t_start = t_start
+        self.t_end = t_end
+
+        self.slice_num = None
+        self.ts = []
+
+
+        self.Inphase = []
+        self.Quadrature = []
+
+
+        self.output = []
+
+
+    def combine(self, res_key):
+
+        self.calc_slice_num(res_key)
+        self.create_ts(res_key)
+
+
+        ts = self.ts
+
+        carrier_group = self.ressource_groups["carrier"]
+
+
+        signal = self.ressources[0]
+        for comp in signal.comps:
+            if carrier_group in comp.groups:
+                omega_lo = comp.params["freq"]
+
+
+        self.output = np.zeros_like(ts)
+
+        self.output += (self.Inphase * np.cos(omega_lo * ts) +
+                        self.Quadrature * np.sin(omega_lo * ts))
+
+
+class AWG(Device):
+    """
+    """
+    def __init__(
+            self,
+            t_start = None,
+            t_end = None,
+            ):
+
+        self.t_start = t_start
+        self.t_end = t_end
+
+        self.slice_num = None
+        self.ts = []
+
+        self.Inphase = []
+        self.Quadrature = []
+        self.amp_tot_sq = None
+
+
+    def create_IQ(self, res_key):
+        """
+        Construct the in-phase (I) and quadrature (Q) components of the
+
+        control signals. These are universal to either experiment or
+        simulation. In the experiment these will be routed to AWG and mixer
+        electronics, while in the simulation they provide the shapes of the
+        controlfields to be added to the Hamiltonian.
+
+        """
+
+        self.calc_slice_num(res_key)
+        self.create_ts(res_key)
+
+        ts = self.ts
+
+
+        Inphase = []
+        Quadrature = []
+
+        comp_group = self.ressource_groups["comp"]
+
+
+        amp_tot_sq = 0
+        components = []
+
+        signal = self.ressources[0]
+        for comp in signal.comps:
+            if comp_group in comp.groups:
+
+                amp = comp.params['amp']
+
+                amp_tot_sq += amp**2
+
+                xy_angle = comp.params['xy_angle']
+                freq_offset = comp.params['freq_offset']
+                components.append(
+                    amp * comp.get_shape_values(ts) *
+                    np.exp(1j * (xy_angle + freq_offset * ts))
+                    )
+
+        norm = np.sqrt(amp_tot_sq)
+        Inphase = np.real(np.sum(components, axis = 0)) / norm
+        Quadrature = np.imag(np.sum(components, axis = 0)) / norm
+
+        self.amp_tot_sq = amp_tot_sq
+        self.Inphase = Inphase
+        self.Quadrature = Quadrature
+
+
+    def get_I(self):
+        return self.amp_tot_sq * self.Inphase
+
+
+    def get_Q(self):
+        return self.amp_tot_sq * self.Quadrature
+
+
+
+    def plot_IQ_components(self, res_key):
+        """ Plotting control functions """
+
+        ts = self.ts
+        res = self.resolutions[res_key]
+        I = self.Inphase
+        Q = self.Quadrature
+
+        plt.rcParams['figure.dpi'] = 100
+        fig, axs = plt.subplots(2, 1)
+
+        axs[0].plot(ts * res, I)
+        axs[1].plot(ts * res, Q)
+        # I (Kevin) don't really understand the behaviour of plt.show()
+        # here. If I only put plt.show(block=False), I get error messages
+        # on my system at home. Adding a second plt.show() resolves that
+        # issue???
+        # look at:  https://github.com/matplotlib/matplotlib/issues/12692/
+        plt.show(block=False)
+        plt.show()
+
+
+
+    # def plot_IQ(self, ts, Is, Qs):
+        # """
+        # Plot (hopefully) into an existing figure.
+        # """
+        # plt.cla()
+        # plt.plot(ts / 1e-9, Is)
+        # plt.plot(ts / 1e-9, Qs)
+        # plt.legend(("I", "Q"))
+        # plt.ylabel('I/Q')
+        # plt.xlabel('Time[ns]')
+        # plt.tick_params('both',direction='in')
+        # plt.tick_params('both', direction='in')
+
+
+
+
+#     def plot_fft_IQ_components(self, axs=None):
+
+
+        # print("WARNING: still have to adjust the x-axis")
+
+        # """ Plotting control functions """
+        # plt.rcParams['figure.dpi'] = 100
+        # IQ = self.get_IQ()
+        # fft_IQ = {}
+        # fft_I = np.fft.fft(IQ['I'])
+        # fft_Q = np.fft.fft(IQ['Q'])
+        # fft_IQ['I'] = np.fft.fftshift(fft_I.real / max(fft_I.real))
+        # fft_IQ['Q'] = np.fft.fftshift(fft_Q.real / max(fft_Q.real))
+
+        # fig, axs = plt.subplots(2, 1)
+        # axs[0].plot(self.ts[0] * self.res[0], fft_IQ['I'])
+        # axs[1].plot(self.ts[0] * self.res[0], fft_IQ['Q'])
+        # # I (Kevin) don't really understand the behaviour of plt.show()
+        # # here. If I only put plt.show(block=False), I get error messages
+        # # on my system at home. Adding a second plt.show() resolves that
+        # # issue???
+        # # look at:  https://github.com/matplotlib/matplotlib/issues/12692/
+        # plt.show(block=False)
+        # plt.show()
+
+
+
+class Generator:
+    """
+    """
+    def __init__(
+            self,
+            devices = {}
+            ):
+
+        self.devices = devices
+
+
+    def generate_signal(self):
+        ####
+        #
+        # PLACEHOLDER
+        # HAS TO BE CODED BY THE USER. BUT HAS TO RETURN np.array OF SIGNAL
+        #
+        ####
+        raise NotImplementedError()
+
+
+
--- c3po/signals/signal.py
+++ c3po/signals/signal.py
@@ -9,9 +9,11 @@ class Signal:
     """
     def __init__(
             self,
+            name = " ",
+            desc = " ",
+            comment = " ",
             t_start = None,
             t_end = None,
-            res = [],
             comps = []
             ):
 
@@ -20,15 +22,14 @@ class Signal:
         # https://docs.python.org/2/library/uuid.html#uuid.uuid4
         self.__uuid = uuid.uuid4()
 
+
+        self.name = name
+        self.desc = desc
+        self.comment = comment
+
         self.t_start = t_start
         self.t_end = t_end
-        self.res = res
 
-        self.slice_num = []
-        self.calc_slice_num()
-
-        self.ts = []
-        self.create_ts()
 
         self.comps = comps
 
@@ -42,19 +43,6 @@ class Signal:
 
     def set_uuid(self, uuid):
         self.__uuid = uuid
-
-
-    def calc_slice_num(self):
-        if self.t_start != None and self.t_end != None and self.res != []:
-            for r in self.res:
-                self.slice_num.append(int(
-                    np.abs(self.t_start - self.t_end) * r) + 1)
-
-
-    def create_ts(self):
-        if self.t_start != None and self.t_end != None and self.slice_num != []:
-            for num in self.slice_num:
-                self.ts.append(np.linspace(self.t_start, self.t_end, num))
 
 
     def get_parameter_value(self, key, uuid):
@@ -119,177 +107,3 @@ class Signal:
 
         return opt_map
 
-    def generate_signal(self):
-        ####
-        #
-        # PLACEHOLDER
-        # HAS TO BE CODED BY THE USER. BUT HAS TO RETURN np.array OF SIGNAL
-        #
-        ####
-        raise NotImplementedError()
-
-
-    def plot_signal(self):
-        """ Plotting control functions """
-        plt.rcParams['figure.dpi'] = 100
-        signal = self.generate_signal()
-        fig = plt.figure()
-        ax = fig.add_subplot(1, 1, 1)
-        ax.plot(self.ts[1] * self.res[0], signal)
-        ax.set_xlabel('Time [ns]')
-    #    plt.show(block=False)
-
-
-    def plot_fft_signal(self):
-
-        print("WARNING: still have to adjust the x-axis")
-
-        """ Plotting control functions """
-        plt.rcParams['figure.dpi'] = 100
-        signal = self.generate_signal()
-
-        fft_signal = np.fft.fft(signal)
-        fft_signal = np.fft.fftshift(fft_signal.real / max(fft_signal.real))
-
-        plt.plot(self.ts[1] * self.res[0], fft_signal)
-
-        plt.show(block=False)
-        plt.show()
-
-
-
-####
-#
-# CHILD CLASS OF class Signal
-#
-####
-class IQ(Signal):
-    def get_IQ(self, carrier_uuid = None):
-        """
-        Construct the in-phase (I) and quadrature (Q) components of the
-
-        control signals. These are universal to either experiment or
-        simulation. In the experiment these will be routed to AWG and mixer
-        electronics, while in the simulation they provide the shapes of the
-        controlfields to be added to the Hamiltonian.
-
-        """
-        signal = {}
-
-        Inphase = []
-        Quadrature = []
-
-        for comp in self.comps:
-            # Identification via desc
-            if comp.desc == "carrier":
-                carrier = comp
-
-            # Identification via id
-            if carrier_uuid is not None and comp.get_uuid() == carrier_uuid:
-                carrier = comp
-
-
-        omega_d = carrier.params['freq']
-
-        amp_tot_sq = 0
-        components = []
-        for comp in self.comps:
-            if "carrier" in comp.desc:
-                continue
-
-            amp = comp.params['amp']
-
-            amp_tot_sq += amp**2
-
-            xy_angle = comp.params['xy_angle']
-            freq_offset = comp.params['freq_offset']
-            components.append(
-                amp * comp.get_shape_values(self.ts[0]) *
-                np.exp(1j * (xy_angle + freq_offset * self.ts[0]))
-                )
-
-        norm = np.sqrt(amp_tot_sq)
-        Inphase = np.real(np.sum(components, axis = 0)) / norm
-        Quadrature = np.imag(np.sum(components, axis = 0)) / norm
-
-        signal['omega'] = omega_d
-        signal['amp'] = amp_tot_sq
-        signal['I'] = Inphase
-        signal['Q'] = Quadrature
-
-        return signal
-
-
-
-    def generate_signal(self):
-        IQ = self.get_IQ()
-        """
-        """
-
-        AWG_I = np.interp(self.ts[1], self.ts[0], IQ['I'])
-        AWG_Q = np.interp(self.ts[1], self.ts[0], IQ['Q'])
-        amp = IQ['amp']
-        omega_d = IQ['omega']
-
-        sig = np.zeros_like(self.ts[1])
-
-        sig += amp * (AWG_I * np.cos(omega_d * self.ts[1]) +
-                      AWG_Q * np.sin(omega_d * self.ts[1]))
-
-        return sig
-
-
-    def plot_IQ_components(self):
-        """ Plotting control functions """
-
-        IQ = self.get_IQ()
-        plt.rcParams['figure.dpi'] = 100
-        fig, axs = plt.subplots(2, 1)
-        axs[0].plot(self.ts[0] * self.res[0], IQ['I'])
-        axs[1].plot(self.ts[0] * self.res[0], IQ['Q'])
-        # I (Kevin) don't really understand the behaviour of plt.show()
-        # here. If I only put plt.show(block=False), I get error messages
-        # on my system at home. Adding a second plt.show() resolves that
-        # issue???
-        # look at:  https://github.com/matplotlib/matplotlib/issues/12692/
-        plt.show(block=False)
-        plt.show()
-
-    def plot_IQ(self, ts, Is, Qs):
-        """
-        Plot (hopefully) into an existing figure.
-        """
-        plt.cla()
-        plt.plot(ts / 1e-9, Is)
-        plt.plot(ts / 1e-9, Qs)
-        plt.legend(("I", "Q"))
-        plt.ylabel('I/Q')
-        plt.xlabel('Time[ns]')
-        plt.tick_params('both',direction='in')
-        plt.tick_params('both', direction='in')
-
-
-    def plot_fft_IQ_components(self, axs=None):
-
-
-        print("WARNING: still have to adjust the x-axis")
-
-        """ Plotting control functions """
-        plt.rcParams['figure.dpi'] = 100
-        IQ = self.get_IQ()
-        fft_IQ = {}
-        fft_I = np.fft.fft(IQ['I'])
-        fft_Q = np.fft.fft(IQ['Q'])
-        fft_IQ['I'] = np.fft.fftshift(fft_I.real / max(fft_I.real))
-        fft_IQ['Q'] = np.fft.fftshift(fft_Q.real / max(fft_Q.real))
-
-        fig, axs = plt.subplots(2, 1)
-        axs[0].plot(self.ts[0] * self.res[0], fft_IQ['I'])
-        axs[1].plot(self.ts[0] * self.res[0], fft_IQ['Q'])
-        # I (Kevin) don't really understand the behaviour of plt.show()
-        # here. If I only put plt.show(block=False), I get error messages
-        # on my system at home. Adding a second plt.show() resolves that
-        # issue???
-        # look at:  https://github.com/matplotlib/matplotlib/issues/12692/
-        plt.show(block=False)
-        plt.show()
