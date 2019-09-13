8c438b878a8bbb013b1cdab57cce1d65fdf163a6
Nico <s9niwitt@stud.uni-saarland.de>
2019-09-13 16:42:29 +0200

    Updated dependencies.

--- setup.py
+++ setup.py
@@ -5,4 +5,11 @@ setup(
     version='0.1dev',
     packages=['c3po', 'c3po/optimizer', 'c3po/cobj', ],
     long_description=open('README.md').read(),
+    install_requires=[
+        'matplotlib',
+        'numpy',
+        'qutip',
+        'tensorflow',
+        'uuid'
+    ]
 )
