d630db890f1c6603d50456f2a72d00ed052cc7b6
Nico <s9niwitt@stud.uni-saarland.de>
2019-10-22 15:50:57 +0200

    Updating unittests.

--- c3po/tests/test_model_unittest.py
+++ c3po/tests/test_model_unittest.py
@@ -3,21 +3,28 @@ import numpy as np
 from c3po.component import Qubit as Qubit
 from c3po.model import Model as mdl
 
+
 class TestModel(unittest.TestCase):
 
     def test_qubit(self):
         q1 = Qubit(
-                name = "Q1",
-                desc = "Qubit 1",
-                comment = "",
-                freq = 5e9*2*np.pi,
-                anhar = -150e6*2*np.pi,
-                hilbert_dim = 3
-            )
+            name="Q1",
+            desc="Qubit 1",
+            comment="",
+            freq=5e9 * 2 * np.pi,
+            anhar=-150e6 * 2 * np.pi,
+            hilbert_dim=3
+        )
         chip_elements = [q1]
-        model = mdl(chip_elements, 0.7e9*2*np.pi)
-        self.assertCountEqual(model.params, [5e9*2*np.pi, -150e6*2*np.pi])
-        self.assertCountEqual(model.params_desc, [['Q1', 'freq'], ['Q1', 'anhar']])
+        model = mdl(chip_elements, 0.7e9 * 2 * np.pi)
+        self.assertCountEqual(
+            model.params, [5e9 * 2 * np.pi, -150e6 * 2 * np.pi]
+        )
+
+        self.assertCountEqual(
+            model.params_desc, [['Q1', 'freq'], ['Q1', 'anhar']]
+        )
+
 
 if __name__ == '__main__':
     unittest.main()
