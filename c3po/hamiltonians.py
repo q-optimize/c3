7f7691ef612dd6071ef4212fbe216cd765621001
fedroy <federicoaroy@gmail.com>
2019-11-22 14:40:16 +0100

    Y drive and coupling

--- c3po/hamiltonians.py
+++ c3po/hamiltonians.py
@@ -63,6 +63,28 @@ def int_XX(anhs):
     return np.matmul(a_dag + a, b_dag + b)
 
 
+def int_YY(anhs):
+    """
+    Dipole type coupling.
+
+    Parameters
+    ----------
+    anhs : Tensor list
+        Annihilators.
+
+    Returns
+    -------
+    Tensor
+        coupling
+
+    """
+    a = anhs[0]
+    b = anhs[1]
+    a_dag = a.T.conj()
+    b_dag = b.T.conj()
+    return -np.matmul(a_dag - a, b_dag - b)
+
+
 def x_drive(a):
     """
     Semiclassical drive.
@@ -80,3 +102,22 @@ def x_drive(a):
     """
     a_dag = a.T.conj()
     return a_dag + a
+
+
+def y_drive(a):
+    """
+    Semiclassical drive.
+
+    Parameters
+    ----------
+    a : Tensor
+        Annihilator.
+
+    Returns
+    -------
+    Tensor
+        Number operator.
+
+    """
+    a_dag = a.T.conj()
+    return 1.0j * (a_dag - a)
