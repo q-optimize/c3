2e5d2b4921e8adea1301273f9d63c8e998ce2c77
fedroy <federicoaroy@gmail.com>
2020-01-20 18:03:17 +0000

    added get_drift_eigenfram to model

--- c3po/model.py
+++ c3po/model.py
@@ -240,6 +240,20 @@ class Model:
 
         return drift_H, self.control_Hs
 
+    def get_drift_eigenframe(self, params=None):
+        if params is None:
+            params = self.params
+
+        drift_H = tf.zeros_like(self.drift_Hs[0])
+        for ii in range(self.n_params):
+            drift_H += \
+                tf.cast(params[ii], tf.complex128) * self.drift_Hs[ii]
+
+        e,v = tf.linalg.eigh(drift_H)
+        eigenframe = tf.zeros_like(drift_H)
+        eigenframe = tf.linalg.set_diag(eigenframe, e)
+        return eigenframe
+
     def get_Virtual_Z(self, t_final):
         anns = []
         freqs = []
