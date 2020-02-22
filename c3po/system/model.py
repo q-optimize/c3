fixed frame rotation to include freq_offset

c3po/system/model.py
@@ -188,14 +188,14 @@ class Model:
     def get_Frame_Rotation(
         self,
         t_final: np.float64,
-        lo_freqs: dict,
+        freqs: dict,
         framechanges: dict
     ):
-        # lo_freqs need to be ordered the same as the names of the qubits
+        # freqs need to be ordered the same as the names of the qubits
         ones = tf.ones(self.tot_dim, dtype=tf.complex128)
         FR = tf.linalg.diag(ones)
-        for line in lo_freqs.keys():
-            lo_freq = lo_freqs[line]
+        for line in freqs.keys():
+            freq = freqs[line]
             framechange = framechanges[line]
             qubit = self.couplings[line].connected[0]
             ann_oper = self.ann_opers[qubit]
@@ -204,7 +204,7 @@ class Model:
                 dtype=tf.complex128
             )
             FR = FR * tf.linalg.expm(
-                1.0j * num_oper * (lo_freq * t_final + framechange)
+                1.0j * num_oper * (freq * t_final + framechange)
             )
         if self.dressed:
             FR = tf.matmul(tf.matmul(