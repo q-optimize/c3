1df08b4f0767b0dc175183d4d0edf6f983ab807e
fedroy <federicoaroy@gmail.com>
2020-02-26 22:00:41 +0100

    c2_run temporarily adapted for synthetic

--- c3po/runs/c2_run.py
+++ c3po/runs/c2_run.py
@@ -2,7 +2,6 @@
 
 import os
 import json
-import pickle
 import argparse
 import c3po.utils.parsers as parsers
 import c3po.utils.tf_utils as tf_utils
@@ -22,7 +21,7 @@ eval_func = cfg['eval_func']
 tf_utils.tf_setup()
 with tf.device('/CPU:0'):
     exp = parsers.create_experiment(exp_setup)
-    opt = parsers.create_c2_opt(opt_config, eval_func)
+    opt, exp_right = parsers.create_c2_opt(opt_config, eval_func)
     opt.set_exp(exp)
     dir = opt.logdir
 
@@ -36,5 +35,15 @@ with tf.device('/CPU:0'):
     os.system('cp {} {}/{}'.format(exp_setup, dir, base(exp_setup)))
     os.system('cp {} {}/{}'.format(opt_config, dir, base(opt_config)))
     os.system('cp {} {}/{}'.format(eval_func, dir, base(eval_func)))
+    real = {'params': [
+        par.numpy().tolist()
+        for par in exp_right.get_parameters()]
+    }
+    with open(dir + "real_model_params.log", 'w') as real_file:
+        real_file.write(json.dumps(exp_right.id_list))
+        real_file.write("\n")
+        real_file.write(json.dumps(real))
+        real_file.write("\n")
+        real_file.write(exp_right.print_parameters())
 
     opt.optimize_controls()
