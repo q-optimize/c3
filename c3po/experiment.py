Fixed frame rotation and added sqrt to newest estimator (gLLprime)

c3po/experiment.py
@@ -160,9 +160,9 @@ class Experiment:
                     if "gauss" in ctrls:
                         if ctrls['gauss'].params["amp"] != 0.0:
                             offset = ctrls['gauss'].params['freq_offset'].get_value()
-                    if "nodrive_env" in ctrls:
-                        offset = ctrls['nodrive_env'].params['freq_offset'].get_value()
-                    # print("gate: ", gate, "; line: ", line, "; offset: ", offset)
+#                     if "nodrive_env" in ctrls:
+                        # offset = ctrls['nodrive_env'].params['freq_offset'].get_value()
+#                     # print("gate: ", gate, "; line: ", line, "; offset: ", offset)
                     freqs[line] = tf.cast(
                         ctrls['carrier'].params['freq'].get_value()
                         + offset,

c3po/libraries/estimators.py
@@ -172,7 +172,7 @@ def g_LL_prime(exp_values, sim_values, exp_stds, shots):
     K = len(sim_values)
     print("K: " + str(K))
     loglkh = - (1 / K) * loglkh
-    return loglkh
+    return tf.sqrt(loglkh)
 
 
 @estimator_reg_deco