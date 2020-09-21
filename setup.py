1d909f792c964d50af5eee5206c8dcdaed6c8110
Nico <s9niwitt@stud.uni-saarland.de>
2020-09-21 11:24:22 +0200

    Entries for setup file.

--- setup.py
+++ setup.py
@@ -4,14 +4,7 @@ setup(
     name='c3po',
     version='1.0rc',
     packages=[
-        'c3po',
-        'c3po/utils',
-        'c3po/system',
-        'c3po/libraries',
-        'c3po/optimizers',
-        'c3po/runs',
-        'c3po/signal',
-        'c3po/generator'
+        'c3po'
     ],
     long_description=open('README.md').read(),
     install_requires=[
