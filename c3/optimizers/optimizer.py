701ef18b3448bd22ac98e750b638473101307086
lazyoracle <contact@anuragsaharoy.me>
2020-11-28 19:51:57 +0000

    add type annotations

--- c3/optimizers/optimizer.py
+++ c3/optimizers/optimizer.py
@@ -30,7 +30,7 @@ class Optimizer:
         algorithm=None,
         plot_dynamics=False,
         plot_pulses=False,
-        store_unitaries=False
+        store_unitaries=False,
     ):
         self.pmap = pmap
         self.optim_status = {}
@@ -59,8 +59,8 @@ class Optimizer:
         """
         old_logdir = self.logdir
         self.logdir = new_logdir
-        os.remove(self.dir_path + '/recent')
-        #os.remove(self.dir_path + self.string)
+        os.remove(self.dir_path + "/recent")
+        # os.remove(self.dir_path + self.string)
         os.rmdir(old_logdir)
 
     def set_exp(self, exp):
@@ -85,10 +85,8 @@ class Optimizer:
         with open(init_point) as init_file:
             best = hjson.load(init_file)
 
-        best_opt_map = [
-            [tuple(par) for par in pset] for pset in best["opt_map"]
-        ]
-        init_p = best['optim_status']['params']
+        best_opt_map = [[tuple(par) for par in pset] for pset in best["opt_map"]]
+        init_p = best["optim_status"]["params"]
         self.pmap.set_parameters(init_p, best_opt_map)
 
     def start_log(self):
@@ -98,7 +96,7 @@ class Optimizer:
         """
         self.start_time = time.time()
         start_time_str = str(f"{time.asctime(time.localtime())}\n\n")
-        with open(self.logdir + self.logname, 'a') as logfile:
+        with open(self.logdir + self.logname, "a") as logfile:
             logfile.write("Starting optimization at ")
             logfile.write(start_time_str)
             logfile.write("Optimization parameters:\n")
@@ -118,20 +116,16 @@ class Optimizer:
 
         """
         self.end_time = time.time()
-        with open(self.logdir + self.logname, 'a') as logfile:
-            logfile.write(
-                f"Finished at {time.asctime(time.localtime())}\n"
-            )
-            logfile.write(
-                f"Total runtime: {self.end_time-self.start_time}\n\n"
-            )
+        with open(self.logdir + self.logname, "a") as logfile:
+            logfile.write(f"Finished at {time.asctime(time.localtime())}\n")
+            logfile.write(f"Total runtime: {self.end_time-self.start_time}\n\n")
             logfile.flush()
 
     def log_best_unitary(self):
         """
         Save the best unitary in the log.
         """
-        with open(self.logdir + 'best_point_' + self.logname, 'w') as best_point:
+        with open(self.logdir + "best_point_" + self.logname, "w") as best_point:
             U_dict = self.exp.unitaries
             for gate, U in U_dict.items():
                 best_point.write("\n")
@@ -147,47 +141,46 @@ class Optimizer:
         Call plotting functions as set up.
 
         """
-        if self.optim_status['goal'] < self.current_best_goal:
-            self.current_best_goal = self.optim_status['goal']
-            self.current_best_params = self.optim_status['params']
-            with open(
-                self.logdir + 'best_point_' + self.logname, 'w'
-            ) as best_point:
+        if self.optim_status["goal"] < self.current_best_goal:
+            self.current_best_goal = self.optim_status["goal"]
+            self.current_best_params = self.optim_status["params"]
+            with open(self.logdir + "best_point_" + self.logname, "w") as best_point:
                 best_dict = {
                     "opt_map": self.pmap.opt_map,
                     "units": self.pmap.get_opt_units(),
-                    "optim_status": self.optim_status
+                    "optim_status": self.optim_status,
                 }
                 best_point.write(hjson.dumps(best_dict))
                 best_point.write("\n")
         if self.plot_dynamics:
             psi_init = self.pmap.model.tasks["init_ground"].initialise(
-                self.pmap.model.drift_H,
-                self.pmap.model.lindbladian
+                self.pmap.model.drift_H, self.pmap.model.lindbladian
             )
-            #dim = np.prod(self.pmap.model.dims)
-            #psi_init = [0] * dim
-            #psi_init[1] = 1
-            #psi_init = tf.constant(psi_init, dtype=tf.complex128, shape=[dim ,1])
+            # dim = np.prod(self.pmap.model.dims)
+            # psi_init = [0] * dim
+            # psi_init[1] = 1
+            # psi_init = tf.constant(psi_init, dtype=tf.complex128, shape=[dim ,1])
             for gate in self.exp.dUs.keys():
-                self.exp.plot_dynamics(psi_init, [gate], self.optim_status['goal'])
+                self.exp.plot_dynamics(psi_init, [gate], self.optim_status["goal"])
             self.exp.dynamics_plot_counter += 1
         if self.plot_pulses:
             for gate in self.opt_gates:
                 instr = self.pmap.instructions[gate]
-                self.exp.plot_pulses(instr, self.optim_status['goal'])
+                self.exp.plot_pulses(instr, self.optim_status["goal"])
             self.exp.pulses_plot_counter += 1
         if self.store_unitaries:
-            self.exp.store_Udict(self.optim_status['goal'])
+            self.exp.store_Udict(self.optim_status["goal"])
             self.exp.store_unitaries_counter += 1
-        with open(self.logdir + self.logname, 'a') as logfile:
-            logfile.write(f"\nFinished evaluation {self.evaluation} at {time.asctime()}\n")
+        with open(self.logdir + self.logname, "a") as logfile:
+            logfile.write(
+                f"\nFinished evaluation {self.evaluation} at {time.asctime()}\n"
+            )
             # logfile.write(hjson.dumps(self.optim_status, indent=2))
             logfile.write(hjson.dumps(self.optim_status))
             logfile.write("\n")
             logfile.flush()
 
-    def fct_to_min(self, x):
+    def fct_to_min(self, x: np.array) -> np.array:
         """
         Wrapper for the goal function.
 
@@ -202,7 +195,8 @@ class Optimizer:
             Value of the goal function.
         """
         current_params = tf.constant(x)
-        goal = self.goal_run(current_params)
+        # TODO Why does mypy through an AttributeNotFound error?
+        goal = self.goal_run(current_params)  # type: ignore
         self.log_parameters()
         if isinstance(goal, tf.Tensor):
             goal = float(goal.numpy())
@@ -210,17 +204,17 @@ class Optimizer:
 
     def fct_to_min_autograd(self, x):
         """
-        Wrapper for the goal function, including evaluation and storage of the gradient.
+         Wrapper for the goal function, including evaluation and storage of the gradient.
 
-       Parameters
-        ----------
-        x : np.array
-            Vector of parameters in the optimizer friendly way.
+        Parameters
+         ----------
+         x : np.array
+             Vector of parameters in the optimizer friendly way.
 
-        Returns
-        -------
-        float
-            Value of the goal function.
+         Returns
+         -------
+         float
+             Value of the goal function.
         """
         current_params = tf.constant(x)
         goal, grad = self.goal_run_with_grad(current_params)
@@ -228,7 +222,7 @@ class Optimizer:
             grad = grad.numpy()
         gradients = grad.flatten()
         self.gradients[str(current_params.numpy())] = gradients
-        self.optim_status['gradient'] = gradients.tolist()
+        self.optim_status["gradient"] = gradients.tolist()
         self.log_parameters()
         if isinstance(goal, tf.Tensor):
             goal = float(goal.numpy())
