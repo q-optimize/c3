001d13e45245dbb968afb1f13f0be6464f918e00
lazyoracle <contact@anuragsaharoy.me>
2021-01-10 23:05:39 +0530

    check transpiling and jobs

--- test/test_qiskit.py
+++ test/test_qiskit.py
@@ -2,6 +2,10 @@
 """
 
 from c3.qiskit import C3Provider
+from c3.qiskit.c3_job import C3Job
+from qiskit.quantum_info import Statevector
+from qiskit import transpile
+from test.conftest import get_test_circuit  # noqa
 from qiskit.providers import BackendV1 as Backend
 
 import pytest
@@ -27,6 +31,47 @@ def test_get_backend(backend):
     backend : str
         name of the backend that is to be fetched
     """
-    qtf = C3Provider()
-    received_backend = qtf.get_backend(backend)
+    c3_qiskit = C3Provider()
+    received_backend = c3_qiskit.get_backend(backend)
     assert isinstance(received_backend, Backend)
+
+
+@pytest.mark.unit
+@pytest.mark.qiskit
+@pytest.mark.parametrize("backend", ["c3_qasm_simulator"])
+def test_transpile(get_test_circuit, backend):  # noqa
+    """Test the transpiling using our backends
+
+    Parameters
+    ----------
+    get_test_circuit : callable
+        pytest fixture for a simple quantum circuit
+    backend : str
+        name of the backend which is to be tested
+    """
+    c3_qiskit = C3Provider()
+    received_backend = c3_qiskit.get_backend(backend)
+    trans_qc = transpile(get_test_circuit, received_backend)
+    assert Statevector.from_instruction(get_test_circuit).equiv(
+        Statevector.from_instruction(trans_qc)
+    )
+
+
+@pytest.mark.integration
+@pytest.mark.qiskit
+@pytest.mark.parametrize("backend", ["c3_qasm_simulator"])
+def test_run_job(get_test_circuit, backend):  # noqa
+    """Test if Backend.run() gives a Job instance
+
+    Parameters
+    ----------
+    get_test_circuit : callable
+        pytest fixture for a simple quantum circuit
+    backend : str
+        name of the backend which is to be tested
+    """
+    c3_qiskit = C3Provider()
+    received_backend = c3_qiskit.get_backend(backend)
+    trans_qc = transpile(get_test_circuit, received_backend)
+    job = received_backend.run(trans_qc)
+    assert isinstance(job, C3Job)
