6c780805d05c3e3a502c2050371a4b912720dae1
lazyoracle <contact@anuragsaharoy.me>
2021-01-13 01:20:33 +0530

    bell circuit

--- test/conftest.py
+++ test/conftest.py
@@ -9,9 +9,26 @@ def get_test_circuit() -> QuantumCircuit:
     Returns
     -------
     QuantumCircuit
+        A circuit with a Hadamard, a C-X
+    """
+    qc = QuantumCircuit(2, 2)
+    qc.h(0)
+    qc.cx(0, 1)
+    return qc
+
+
+@pytest.fixture()
+def get_bell_circuit() -> QuantumCircuit:
+    """fixture for Quantum Circuit to make Bell
+    State |11> + |00>
+
+    Returns
+    -------
+    QuantumCircuit
         A circuit with a Hadamard, a C-X and 2 Measures
     """
     qc = QuantumCircuit(2, 2)
     qc.h(0)
     qc.cx(0, 1)
+    qc.measure([0, 1], [0, 1])
     return qc
