0a188aacac80d7a0016e7f087a6b0f805f9175cf
Nicolas Wittler <n.wittler@pm.me>
2021-02-04 11:51:28 +0100

    sensible defaults for amps

--- c3/signal/pulse.py
+++ c3/signal/pulse.py
@@ -30,54 +30,24 @@ class Envelope(C3obj):
     """
 
     def __init__(
-            self,
-            name: str,
-            desc: str = " ",
-            comment: str = " ",
-            params: dict = {},
-            shape: types.FunctionType = None,
+        self,
+        name: str,
+        desc: str = " ",
+        comment: str = " ",
+        params: dict = {},
+        shape: types.FunctionType = None,
     ):
         if isinstance(shape, str):
             self.shape = envelopes[shape]
         else:
             self.shape = shape
         params_default = {
-            'amp': Qty(
-                value=0.0,
-                min_val=-1.0,
-                max_val=+1.0,
-                unit="V"
-            ),
-            'delta': Qty(
-                value=0.0,
-                min_val=-5.0,
-                max_val=+5.0,
-                unit="V"
-            ),
-            'freq_offset': Qty(
-                value=0.0,
-                min_val=-1.0,
-                max_val=+1.0,
-                unit='Hz 2pi'
-            ),
-            'xy_angle': Qty(
-                value=0.0,
-                min_val=-1.0,
-                max_val=+1.0,
-                unit='rad'
-            ),
-            'sigma': Qty(
-                value=5e-9,
-                min_val=-2.0,
-                max_val=+2.0,
-                unit="s"
-            ),
-            't_final': Qty(
-                value=0.0,
-                min_val=-1.0,
-                max_val=+1.0,
-                unit="s"
-            )
+            "amp": Qty(value=1.0, min_val=-1.0, max_val=+1.5, unit="V"),
+            "delta": Qty(value=0.0, min_val=-5.0, max_val=+5.0, unit="V"),
+            "freq_offset": Qty(value=0.0, min_val=-1.0, max_val=+1.0, unit="Hz 2pi"),
+            "xy_angle": Qty(value=0.0, min_val=-1.0, max_val=+1.0, unit="rad"),
+            "sigma": Qty(value=5e-9, min_val=-2.0, max_val=+2.0, unit="s"),
+            "t_final": Qty(value=0.0, min_val=-1.0, max_val=+1.0, unit="s"),
         }
         params_default.update(params)
         super().__init__(
@@ -101,7 +71,7 @@ class Envelope(C3obj):
         return {
             "c3type": self.__class__.__name__,
             "shape": self.shape.__name__,
-            "params": params
+            "params": params,
         }
 
     def __str__(self) -> str:
@@ -117,7 +87,7 @@ class Envelope(C3obj):
         t_before : tf.float64
             Offset the beginning of the shape by this time.
         """
-        t_final = self.params['t_final']
+        t_final = self.params["t_final"]
         if t_before:
             offset = self.shape(t_before, self.params)
             vals = self.shape(ts, self.params) - offset
@@ -141,21 +111,22 @@ class EnvelopeNetZero(Envelope):
         Parameters of the envelope
         Note: t_final
     """
+
     def __init__(
-            self,
-            name: str,
-            desc: str = " ",
-            comment: str = " ",
-            params: dict = {},
-            shape: types.FunctionType = None,
-            ):
+        self,
+        name: str,
+        desc: str = " ",
+        comment: str = " ",
+        params: dict = {},
+        shape: types.FunctionType = None,
+    ):
         super().__init__(
             name=name,
             desc=desc,
             comment=comment,
             params=params,
             shape=shape,
-            )
+        )
 
     def get_shape_values(self, ts, t_before=None):
         """Return the value of the shape function at the specified times.
@@ -170,7 +141,9 @@ class EnvelopeNetZero(Envelope):
         N_red = len(ts) // 2
         ts_red = tf.split(ts, [N_red, len(ts) - N_red], 0)[0]
         shape_values = super().get_shape_values(ts=ts_red, t_before=t_before)
-        netzero_shape_values = tf.concat([shape_values, -shape_values, [0] * (len(ts) % 2)], axis=0)
+        netzero_shape_values = tf.concat(
+            [shape_values, -shape_values, [0] * (len(ts) % 2)], axis=0
+        )
         return netzero_shape_values
 
 
@@ -179,25 +152,15 @@ class Carrier(C3obj):
     """Represents the carrier of a pulse."""
 
     def __init__(
-            self,
-            name: str,
-            desc: str = " ",
-            comment: str = " ",
-            params: dict = {},
+        self,
+        name: str,
+        desc: str = " ",
+        comment: str = " ",
+        params: dict = {},
     ):
         params_default = {
-            'freq': Qty(
-                value=0.0,
-                min_val=-1.0,
-                max_val=+1.0,
-                unit="V"
-            ),
-            'framechange': Qty(
-                value=0.0,
-                min_val=-np.pi,
-                max_val=np.pi,
-                unit="rad"
-            )
+            "freq": Qty(value=0.0, min_val=-1.0, max_val=+1.0, unit="V"),
+            "framechange": Qty(value=0.0, min_val=-np.pi, max_val=np.pi, unit="rad"),
         }
         params_default.update(params)
         super().__init__(
@@ -218,7 +181,4 @@ class Carrier(C3obj):
         params = {}
         for key, item in self.params.items():
             params[key] = item.asdict()
-        return {
-            "c3type": self.__class__.__name__,
-            "params": params
-        }
+        return {"c3type": self.__class__.__name__, "params": params}
