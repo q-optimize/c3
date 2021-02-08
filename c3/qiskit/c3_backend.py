f26bd8aa76535e00136485f84006cdb8ddfeb283
lazyoracle <contact@anuragsaharoy.me>
2021-02-08 13:42:27 +0000

    port backends to base class design

--- c3/qiskit/c3_backend.py
+++ c3/qiskit/c3_backend.py
@@ -20,64 +20,26 @@ from .c3_job import C3Job
 from .c3_backend_utils import get_init_ground_state, get_sequence
 
 from typing import Any, Dict
+from abc import ABC, abstractclassmethod, abstractmethod
 
 logger = logging.getLogger(__name__)
 
 
-class C3QasmSimulator(Backend):
-    """A C3-based Qasm Simulator for Qiskit
+class C3QasmSimulator(Backend, ABC):
+    """An Abtract Base Class for C3 Qasm Simulators for Qiskit.
+    This class CAN NOT be instantiated directly.
 
     Parameters
     ----------
     Backend : qiskit.providers.BackendV1
         The C3QasmSimulator is derived from BackendV1
+    ABC : abc.ABC
+        Helper class for defining Abstract classes using ABCMeta
     """
 
-    MAX_QUBITS_MEMORY = 15
-    _configuration = {
-        "backend_name": "c3_qasm_simulator",
-        "backend_version": "0.1",
-        "n_qubits": min(15, MAX_QUBITS_MEMORY),
-        "url": "https://github.com/q-optimize/c3",
-        "simulator": True,
-        "local": True,
-        "conditional": True,
-        "open_pulse": False,
-        "memory": False,
-        "max_shots": 65536,
-        "coupling_map": None,
-        "description": "A c3 simulator for qasm experiments",
-        "basis_gates": ["u3", "cx", "id", "x"],
-        "gates": [],
-    }
-
-    DEFAULT_OPTIONS = {"initial_statevector": None, "shots": 1024, "memory": False}
-
-    def __init__(self, configuration=None, provider=None, **fields):
-        super().__init__(
-            configuration=(
-                configuration or QasmBackendConfiguration.from_dict(self._configuration)
-            ),
-            provider=provider,
-            **fields
-        )
-        # Define attributes in __init__.
-        self._local_random = np.random.RandomState()
-        self._classical_memory = 0
-        self._classical_register = 0
-        self._statevector = 0
-        self._number_of_cmembits = 0
-        self._number_of_qubits = 0
-        self._shots = 0
-        self._memory = False
-        self._initial_statevector = self.options.get("initial_statevector")
-        self._qobj_config = None
-        # TEMP
-        self._sample_measure = False
-
-    @classmethod
-    def _default_options(cls) -> Options:
-        return Options(shots=1024, memory=False, initial_statevector=None)
+    @abstractclassmethod
+    def _default_options(cls) -> None:
+        raise NotImplementedError("This must be implemented in the derived class")
 
     def set_device_config(self, config_file: str) -> None:
         """Set the path to the config for the device
@@ -186,6 +148,146 @@ class C3QasmSimulator(Backend):
         }
 
         return Result.from_dict(result)
+
+    @abstractmethod
+    def run_experiment(self, experiment: QasmQobjExperiment) -> Dict[str, Any]:
+        raise NotImplementedError("This must be implemented in the derived class")
+
+    def _validate(self, qobj):
+        """Semantic validations of the qobj which cannot be done via schemas."""
+        n_qubits = qobj.config.n_qubits
+        max_qubits = self.configuration().n_qubits
+        if n_qubits > max_qubits:
+            raise C3QiskitError(
+                "Number of qubits {} ".format(n_qubits)
+                + "is greater than maximum ({}) ".format(max_qubits)
+                + 'for "{}".'.format(self.name())
+            )
+        for experiment in qobj.experiments:
+            name = experiment.header.name
+            if experiment.config.memory_slots == 0:
+                logger.warning(
+                    'No classical registers in circuit "%s", ' "counts will be empty.",
+                    name,
+                )
+            elif "measure" not in [op.name for op in experiment.instructions]:
+                logger.warning(
+                    'No measurements in circuit "%s", '
+                    "classical register will remain all zeros.",
+                    name,
+                )
+
+    def _validate_initial_statevector(self):
+        """Raise an error when experiment tries to set initial statevector
+
+        Raises
+        ------
+        C3QiskitError
+            Error for statevector initialisation not implemented
+        """
+        if self._initial_statevector is not None:
+            raise C3QiskitError(
+                "Setting initial statevector is not implemented in this simulator"
+            )
+        else:
+            pass
+
+    def _initialize_statevector(self):
+        """Raise an error when experiment tries to set initial statevector
+
+        Raises
+        ------
+        C3QiskitError
+            Error for statevector initialisation not implemented
+        """
+        if self._initial_statevector is not None:
+            raise C3QiskitError(
+                "Setting initial statevector is not implemented in this simulator"
+            )
+        else:
+            pass
+
+    def _set_options(self, qobj_config=None, backend_options=None):
+        """Qiskit stock method to Set the backend options for all experiments in a qobj"""
+        # Reset default options
+        self._initial_statevector = self.options.get("initial_statevector")
+        if "backend_options" in backend_options and backend_options["backend_options"]:
+            backend_options = backend_options["backend_options"]
+
+        # Check for custom initial statevector in backend_options first,
+        # then config second
+        if "initial_statevector" in backend_options:
+            self._initial_statevector = np.array(
+                backend_options["initial_statevector"], dtype=complex
+            )
+        elif hasattr(qobj_config, "initial_statevector"):
+            self._initial_statevector = np.array(
+                qobj_config.initial_statevector, dtype=complex
+            )
+        if self._initial_statevector is not None:
+            # Check the initial statevector is normalized
+            norm = np.linalg.norm(self._initial_statevector)
+            if round(norm, 12) != 1:
+                raise C3QiskitError(
+                    "initial statevector is not normalized: "
+                    + "norm {} != 1".format(norm)
+                )
+
+
+class C3QasmPerfectSimulator(C3QasmSimulator):
+    """A C3-based perfect gates simulator for Qiskit
+
+    Parameters
+    ----------
+    C3QasmSimulator : c3.qiskit.c3_backend.C3QasmSimulator
+        Inherits the C3QasmSimulator and implements a perfect gate simulator
+    """
+
+    MAX_QUBITS_MEMORY = 20
+    _configuration = {
+        "backend_name": "c3_qasm_perfect_simulator",
+        "backend_version": "0.1",
+        "n_qubits": MAX_QUBITS_MEMORY,
+        "url": "https://github.com/q-optimize/c3",
+        "simulator": True,
+        "local": True,
+        "conditional": False,
+        "open_pulse": False,
+        "memory": False,
+        "max_shots": 65536,
+        "coupling_map": None,
+        "description": "A c3 simulator for qasm experiments with perfect gates",
+        "basis_gates": ["u3", "cx", "id", "x"],
+        "gates": [],
+    }
+
+    DEFAULT_OPTIONS = {"initial_statevector": None, "shots": 1024, "memory": False}
+
+    def __init__(self, configuration=None, provider=None, **fields):
+        super().__init__(
+            configuration=(
+                configuration or QasmBackendConfiguration.from_dict(self._configuration)
+            ),
+            provider=provider,
+            **fields
+        )
+        # Define attributes in __init__.
+        self._local_random = np.random.RandomState()
+        self._classical_memory = 0
+        self._classical_register = 0
+        self._statevector = 0
+        self._number_of_cmembits = 0
+        self._number_of_qubits = 0
+        self._shots = 0
+        self._memory = False
+        self._initial_statevector = self.options.get("initial_statevector")
+        self._qobj_config = None
+        # TEMP
+        self._sample_measure = False
+
+    @classmethod
+    def _default_options(cls) -> Options:
+        return Options(shots=1024, memory=False, initial_statevector=None)
 
     def run_experiment(self, experiment: QasmQobjExperiment) -> Dict[str, Any]:
         """Run an experiment (circuit) and return a single experiment result
@@ -314,83 +416,3 @@ class C3QasmSimulator(Backend):
         }
 
         return exp_result
-
-    def _validate(self, qobj):
-        """Semantic validations of the qobj which cannot be done via schemas."""
-        n_qubits = qobj.config.n_qubits
-        max_qubits = self.configuration().n_qubits
-        if n_qubits > max_qubits:
-            raise C3QiskitError(
-                "Number of qubits {} ".format(n_qubits)
-                + "is greater than maximum ({}) ".format(max_qubits)
-                + 'for "{}".'.format(self.name())
-            )
-        for experiment in qobj.experiments:
-            name = experiment.header.name
-            if experiment.config.memory_slots == 0:
-                logger.warning(
-                    'No classical registers in circuit "%s", ' "counts will be empty.",
-                    name,
-                )
-            elif "measure" not in [op.name for op in experiment.instructions]:
-                logger.warning(
-                    'No measurements in circuit "%s", '
-                    "classical register will remain all zeros.",
-                    name,
-                )
-
-    def _validate_initial_statevector(self):
-        """Raise an error when experiment tries to set initial statevector
-
-        Raises
-        ------
-        C3QiskitError
-            Error for statevector initialisation not implemented
-        """
-        if self._initial_statevector is not None:
-            raise C3QiskitError(
-                "Setting initial statevector is not implemented in this simulator"
-            )
-        else:
-            pass
-
-    def _initialize_statevector(self):
-        """Raise an error when experiment tries to set initial statevector
-
-        Raises
-        ------
-        C3QiskitError
-            Error for statevector initialisation not implemented
-        """
-        if self._initial_statevector is not None:
-            raise C3QiskitError(
-                "Setting initial statevector is not implemented in this simulator"
-            )
-        else:
-            pass
-
-    def _set_options(self, qobj_config=None, backend_options=None):
-        """Qiskit stock method to Set the backend options for all experiments in a qobj"""
-        # Reset default options
-        self._initial_statevector = self.options.get("initial_statevector")
-        if "backend_options" in backend_options and backend_options["backend_options"]:
-            backend_options = backend_options["backend_options"]
-
-        # Check for custom initial statevector in backend_options first,
-        # then config second
-        if "initial_statevector" in backend_options:
-            self._initial_statevector = np.array(
-                backend_options["initial_statevector"], dtype=complex
-            )
-        elif hasattr(qobj_config, "initial_statevector"):
-            self._initial_statevector = np.array(
-                qobj_config.initial_statevector, dtype=complex
-            )
-        if self._initial_statevector is not None:
-            # Check the initial statevector is normalized
-            norm = np.linalg.norm(self._initial_statevector)
-            if round(norm, 12) != 1:
-                raise C3QiskitError(
-                    "initial statevector is not normalized: "
-                    + "norm {} != 1".format(norm)
-                )
