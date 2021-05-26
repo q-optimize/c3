01631cc82b8316571378718c8824f0b345abff55
Nicolas Wittler <n.wittler@pm.me>
2021-05-26 13:08:04 +0200

    Tests for initialization of optim problems

--- test/test_optim_init.py
+++ test/test_optim_init.py
@@ -1,3 +1,6 @@
+"""Test module to check if optimizer classes are initialized correcty by the main file.
+"""
+
 import hjson
 
 from c3.optimizers.c1 import C1
@@ -8,7 +11,25 @@ from c3.main import run_cfg
 def test_main_c1() -> None:
     with open("test/c1.cfg", "r") as cfg_file:
         cfg = hjson.load(cfg_file)
-    run_cfg(cfg, debug=False)
+    run_cfg(cfg, debug=True)
+
+
+def test_main_c2() -> None:
+    with open("test/c2.cfg", "r") as cfg_file:
+        cfg = hjson.load(cfg_file)
+    run_cfg(cfg, debug=True)
+
+
+def test_main_c3() -> None:
+    with open("test/c3.cfg", "r") as cfg_file:
+        cfg = hjson.load(cfg_file)
+    run_cfg(cfg, debug=True)
+
+
+def test_main_sens() -> None:
+    with open("test/sensitivity.cfg", "r") as cfg_file:
+        cfg = hjson.load(cfg_file)
+    run_cfg(cfg, debug=True)
 
 
 def test_create_c1() -> None:
