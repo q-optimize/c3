debe330468ae1d6d755d9a3c4cb8e96e108bd567
frosati1 <f.rosati@fz-juelich.de>
2021-05-26 14:50:16 +0200

    propagation library created and test twoquibts importing fixed

--- test/test_two_qubits.py
+++ test/test_two_qubits.py
@@ -12,21 +12,21 @@ from numpy.testing import assert_array_almost_equal as almost_equal
 from c3.c3objs import Quantity as Qty
 from c3.parametermap import ParameterMap as Pmap
 from c3.experiment import Experiment as Exp
-from c3.system.model import Model as Mdl
+from c3.model import Model as Mdl
 from c3.generator.generator import Generator as Gnr
 
 # Building blocks
 import c3.generator.devices as devices
-import c3.system.chip as chip
+import c3.libraries.chip as chip
 import c3.signal.pulse as pulse
 import c3.signal.gates as gates
-import c3.system.tasks as tasks
 
 # Libs and helpers
 import c3.libraries.algorithms as algorithms
 import c3.libraries.hamiltonians as hamiltonians
 import c3.libraries.fidelities as fidelities
 import c3.libraries.envelopes as envelopes
+import c3.libraries.tasks as tasks
 
 from c3.optimizers.c1 import C1
 
