20ff976f7340979a4106cd38fd9b552d1c29a0e6
Nicolas Wittler <n.wittler@pm.me>
2021-05-28 15:01:31 +0200

    Storing drive info in target

--- c3/model.py
+++ c3/model.py
@@ -66,6 +66,17 @@ class Model:
             self.subsystems[comp.name] = comp
         for comp in couplings:
             self.couplings[comp.name] = comp
+            if isinstance(comp, Drive):  # Check that the target of a drive exists and
+                                         # is store the info in the target.
+                for connect in comp.connected:
+                    try:
+                        self.subsystems[connect].drive_line = comp.name
+                    except KeyError:
+                        raise KeyError(
+                            f"Tried to connect {comp.name}"
+                            f" to non-existent device {self.subsystems[connect].name}."
+                        )
+                        
             if len(set(comp.connected) - set(self.subsystems.keys())) > 0:
                 raise Exception("Tried to connect non-existent devices.")
         # TODO ensure that all elements have different keys / names
