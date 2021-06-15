a2c46f762ccc680f310a14648e6ec3521d53bbee
frosati1 <f.rosati@fz-juelich.de>
2021-06-15 16:53:50 +0200

    used fixtures in test_exp

--- test/test_exp.py
+++ test/test_exp.py
@@ -1,48 +1,32 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[3]:
-
-
-import numpy as np
+"""Testing exponentiantion methods for PWC propagation"""
 import tensorflow as tf
 import pytest
 
 from numpy.testing import assert_array_almost_equal as almost_equal
-from c3.libraries.constants import Id, X, Y, Z
 from c3.libraries.propagation import tf_expm, tf_expm_dynamic
-
-theta = 2 * np.pi * np.random.rand(1)
-"""Testing that exponentiation methods are almost equal in the numpy sense.
-Check that, given P = a*X+b*Y+c*Z with a, b, c random normalized numbers,
-exp(i theta P) = cos(theta)*Id + sin(theta)*P"""
-a = np.random.rand(1)
-b = np.random.rand(1)
-c = np.random.rand(1)
-norm = np.sqrt(a ** 2 + b ** 2 + c ** 2)
-# Normalized random coefficients
-a = a / norm
-b = b / norm
-c = c / norm
-
-P = a * X + b * Y + c * Z
-theta = 2 * np.pi * np.random.rand(1)
-rot = theta * P
-res = np.cos(theta) * Id + 1j * np.sin(theta) * P
 
 
 @pytest.mark.unit
-def test_tf_expm() -> None:
+def test_tf_expm(get_exp_problem) -> None:
+    """Testing tf_expm with fixed number of terms"""
+
+    rot, res = get_exp_problem
     terms = 100
     almost_equal(tf_expm(1j * rot, terms), res)
 
 
 @pytest.mark.unit
 @pytest.mark.skip(reason="experimental: to be tested")
-def test_expm_dynamic() -> None:
+def test_expm_dynamic(get_exp_problem) -> None:
+    """Testing dynamically adjusted exp method"""
+
+    rot, res = get_exp_problem
     almost_equal(tf_expm_dynamic(1j * rot), res)
 
 
 @pytest.mark.unit
-def test_tf_exponentiation() -> None:
+def test_tf_exponentiation(get_exp_problem) -> None:
+    """Testing with the TF exponentiation method"""
+
+    rot, res = get_exp_problem
     almost_equal(tf.linalg.expm(1j * rot), res)
