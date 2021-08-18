ad18e04d135f1228774ad7816735bd4b51696aad
Nicolas Wittler <n.wittler@pm.me>
2021-08-18 11:16:21 +0200

    Checking if called by main.

--- c3/utils/log_reader.py
+++ c3/utils/log_reader.py
@@ -9,21 +9,7 @@ from rich.console import Console
 from rich.table import Table
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("log_file")
-parser.add_argument("-w", "--watch", action="store_true")
-args = parser.parse_args()
-
-log = None
-
-try:
-    with open(args.log_file) as file:
-        log = hjson.load(file, object_pairs_hook=hjson_decode)
-except FileNotFoundError:
-    print("Logfile not found.")
-
-
-def show_table():
+def show_table(log, console) -> None:
     if log:
         opt_map = log["opt_map"]
         optim_status = log["optim_status"]
@@ -35,19 +21,16 @@ def show_table():
         table.add_column("Parameter")
         table.add_column("Value", justify="right")
         table.add_column("Gradient", justify="right")
-        for ii in range(len(opt_map)):
-            equiv_ids = opt_map[ii]
+        for ii, equiv_ids in enumerate(opt_map):
             par = params[ii]
             grad = grads[ii]
             par = num3str(par)
             grad = num3str(grad)
             par_id = equiv_ids[0]
-            nice_id = "-".join(par_id)
-            table.add_row(nice_id, par + units[ii], grad + units[ii])
+            table.add_row(par_id, par + units[ii], grad + units[ii])
             if len(equiv_ids) > 1:
                 for par_id in equiv_ids[1:]:
-                    nice_id = "-".join(par_id)
-                    table.add_row(nice_id, "''", "''")
+                    table.add_row(par_id, "''", "''")
 
         console.clear()
         print(
@@ -56,10 +39,23 @@ def show_table():
         console.print(table)
 
 
-console = Console()
-if args.watch:
-    while True:
-        show_table()
-        time.sleep(5)
-else:
-    show_table()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("log_file")
+    parser.add_argument("-w", "--watch", action="store_true")
+    args = parser.parse_args()
+    log = None
+
+    try:
+        with open(args.log_file) as file:
+            log = hjson.load(file, object_pairs_hook=hjson_decode)
+    except FileNotFoundError:
+        print("Logfile not found.")
+
+    console = Console()
+    if args.watch:
+        while True:
+            show_table(log, console)
+            time.sleep(5)
+    else:
+        show_table(log, console)
