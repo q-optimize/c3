39ed997593caaf6737d8e1ab2bedb2ad6971de96
lazyoracle <contact@anuragsaharoy.me>
2022-01-01 00:09:28 +0100

    init custom qiskit gates

---
+++ c3/qiskit/c3_gates.py
@@ -0,0 +1,54 @@
+from qiskit.circuit import Gate
+from c3.libraries.constants import GATES
+
+
+class C3BaseQiskitGate(Gate):
+    pass
+
+
+class RX90pGate(C3BaseQiskitGate):
+    pass
+
+
+class RX90mGate(C3BaseQiskitGate):
+    pass
+
+
+class RXpGate(C3BaseQiskitGate):
+    pass
+
+
+class RY90pGate(C3BaseQiskitGate):
+    pass
+
+
+class RY90mGate(C3BaseQiskitGate):
+    pass
+
+
+class RYpGate(C3BaseQiskitGate):
+    pass
+
+
+class RZ90pGate(C3BaseQiskitGate):
+    pass
+
+
+class RZ90mGate(C3BaseQiskitGate):
+    pass
+
+
+class RZpGate(C3BaseQiskitGate):
+    pass
+
+
+class CRXpGate(C3BaseQiskitGate):
+    pass
+
+
+class CRGate(C3BaseQiskitGate):
+    pass
+
+
+class CR90Gate(C3BaseQiskitGate):
+    pass
