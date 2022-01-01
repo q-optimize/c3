c602a18e5ac7913928d8b400a01b8f4ceec9d510
lazyoracle <contact@anuragsaharoy.me>
2022-01-01 04:10:32 +0100

    test params for rz90m gate

--- test/test_qiskit.py
+++ test/test_qiskit.py
@@ -188,6 +188,7 @@ def test_too_many_qubits(backend):
         pytest.param(RY90mGate(), [0], RYGate(theta=-np.pi / 2.0), [0], id="ry90m"),
         pytest.param(RYpGate(), [0], RYGate(theta=np.pi), [0], id="ryp"),
         pytest.param(RZ90pGate(), [0], RZGate(phi=np.pi / 2.0), [0], id="rz90p"),
+        pytest.param(RZ90mGate(), [0], RZGate(phi=-np.pi / 2.0), [0], id="rz90m"),
     ],
 )
 @pytest.mark.unit
