Correct masking of multi component signals.

c3/signal/gates.py
@@ -256,6 +256,7 @@ class Instruction:
         else:
             t_end = self.t_end
 
+        # TODO: The following needs to go. We need proper configuration or an error.
         # if t_end > self.t_end:
         #     if self.fixed_t_end and not minimal_time:
         #         warnings.warn(
@@ -296,30 +297,32 @@ class Instruction:
         amp_tot_sq = 0
         signal = tf.zeros_like(ts, tf.complex128)
         self._timings = dict()
-
+        dt = ts[1] - ts[0]
         for comp_name in self.comps[chan]:
             opts = copy.copy(self._options[chan][comp_name])
             opts.update(options)
             comp = self.comps[chan][comp_name]
             t_start, t_end = self.get_timings(chan, comp_name)
+            ts_off = ts - t_start
             if isinstance(comp, Envelope):
 
-                amp = comp.params["amp"].get_value(dtype=tf.complex128)
+                amp_re = comp.params["amp"].get_value()
+                amp = tf.complex(amp_re, tf.zeros_like(amp_re))
 
                 amp_tot_sq += amp**2
 
                 xy_angle = comp.params["xy_angle"].get_value()
                 freq_offset = comp.params["freq_offset"].get_value()
-                phase = -xy_angle - freq_offset * ts
+                phase = -xy_angle - freq_offset * ts_off
                 denv = None
                 if comp.drag or opts.pop("drag", False) or opts.pop("drag_2", False):
-                    dt = ts[1] - ts[0]
+
                     delta = comp.params["delta"].get_value()
                     with tf.GradientTape() as t:
-                        t.watch(ts)
-                        env = comp.get_shape_values(ts=ts)
+                        t.watch(ts_off)
+                        env = comp.get_shape_values(ts_off, t_end - t_start)
                     denv = t.gradient(
-                        env, ts, unconnected_gradients=tf.UnconnectedGradients.ZERO
+                        env, ts_off, unconnected_gradients=tf.UnconnectedGradients.ZERO
                     )  # Derivative W.R.T. to bins
                     if not opts.pop("drag", False):
                         # Use drag_2 definition here
@@ -342,7 +345,7 @@ class Instruction:
                         print("C3 Warning: AWG has less timesteps than given PWC bins")
 
                 else:
-                    env = comp.get_shape_values(ts)
+                    env = comp.get_shape_values(ts_off, t_end - t_start)
                     env = tf.cast(env, tf.complex128)
 
                 signal += (
@@ -372,7 +375,9 @@ class Instruction:
             env_params["freq_offset"] = Quantity(value=sideband, unit="Hz 2pi")
             carrier_freq -= sideband
         self.add_component(
-            comp=Envelope("gaussian", shape=gaussian_nonorm, params=env_params),
+            comp=Envelope(
+                "gaussian", shape=gaussian_nonorm, params=env_params, use_t_before=True
+            ),
             chan=chan,
         )
 

c3/signal/pulse.py
@@ -98,7 +98,7 @@ class Envelope(C3obj):
         else:
             self.get_shape_values = self._get_shape_values_just
 
-    def compute_mask(self, ts) -> tf.Tensor:
+    def compute_mask(self, ts, t_end=0) -> tf.Tensor:
         """Compute a mask to cut out a signal after t_final.
 
         Parameters
@@ -111,11 +111,13 @@ class Envelope(C3obj):
         tf.Tensor
             [description]
         """
-        t_final = self.params["t_final"].get_value()
+        t_final = max(self.params["t_final"].get_value(), t_end)
         dt = ts[1] - ts[0]
-        return tf.sigmoid((0.999 * t_final - ts) / dt * 1e6)
+        return tf.sigmoid((ts / dt + 0.001) * 1e6) * tf.sigmoid(
+            (0.999 * t_final - ts) / dt * 1e6
+        )
 
-    def _get_shape_values_before(self, ts):
+    def _get_shape_values_before(self, ts, t_final=0):
         """Return the value of the shape function at the specified times. With the offset, we make sure the
         signal starts with amplitude zero by subtracting the shape value at time -dt.
 
@@ -126,10 +128,10 @@ class Envelope(C3obj):
         """
         t_before = 2 * ts[0] - ts[1]  # t[0] - (t[1] - t[0])
         offset = self.shape(t_before, self.params)
-        mask = self.compute_mask(ts)
+        mask = self.compute_mask(ts, t_final)
         return mask * (self.shape(ts, self.params) - offset)
 
-    def _get_shape_values_just(self, ts):
+    def _get_shape_values_just(self, ts, t_final=0):
         """Return the value of the shape function at the specified times.
 
         Parameters
@@ -137,7 +139,7 @@ class Envelope(C3obj):
         ts : tf.Tensor
             Vector of time samples.
         """
-        mask = self.compute_mask(ts)
+        mask = self.compute_mask(ts, t_final)
         return mask * self.shape(ts, self.params)
 
 