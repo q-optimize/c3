Fix xy angle sign (#232)
* Changed sign of xy_angle in phase

* Added to changelog

* Created test

* Changed sign of phase in correct_signal to match reality

* Properly use np testing

* Fixed tests to adhere to new phase sign convention

c3/signal/gates.py
@@ -318,7 +318,7 @@ class Instruction:
 
                 xy_angle = comp.params["xy_angle"].get_value()
                 freq_offset = comp.params["freq_offset"].get_value()
-                phase = -xy_angle - freq_offset * ts_off
+                phase = xy_angle - freq_offset * ts_off
                 env = comp.get_shape_values(ts_off, t_end - t_start)
                 env = tf.cast(env, tf.complex128)
 