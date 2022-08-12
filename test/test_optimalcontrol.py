816a1b787489576e50da7b72501bd0e1ac33cb45
Yonatan Gideoni <yonatan.gideoni@gmail.com>
2022-08-12 14:20:43 +0200

    Made test be more general and robust

--- test/test_optimalcontrol.py
+++ test/test_optimalcontrol.py
@@ -1,10 +1,6 @@
-import os
-import tempfile
-
 import numpy as np
 import pytest
 
-import c3.libraries.algorithms as algorithms
 import c3.libraries.fidelities as fidelities
 from c3.optimizers.optimalcontrol import OptimalControl, OptResultOOBError
 from examples.single_qubit_experiment import create_experiment
@@ -12,8 +8,6 @@ from examples.single_qubit_experiment import create_experiment
 
 @pytest.mark.integration
 def test_raises_OOB_for_bad_optimizer() -> None:
-    np.random.seed(0)
-
     exp = create_experiment()
     exp.set_opt_gates(["rx90p[0]"])
 
@@ -32,17 +26,16 @@ def test_raises_OOB_for_bad_optimizer() -> None:
 
     exp.pmap.set_opt_map(gateset_opt_map)
 
-    log_dir = os.path.join(tempfile.TemporaryDirectory().name, "c3logs")
-    cmaes_options = {"spread": 1, "maxiter": 5}
+    def bad_alg(x_init, fun, *args, **kwargs):
+        res = np.array([x * -99 for x in x_init])
+        fun(res)
+        return res
 
     opt = OptimalControl(
-        dir_path=log_dir,
         fid_func=fidelities.unitary_infid_set,
         fid_subspace=["Q1"],
         pmap=exp.pmap,
-        algorithm=algorithms.cmaes,
-        options=cmaes_options,
-        run_name="OOB_CMAES_opt",
+        algorithm=bad_alg,
     )
 
     exp.set_opt_gates(opt_gates)
