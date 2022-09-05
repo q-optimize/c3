Fix silent OOB params post-optimization failure (#236)
* Make Qty raise more specific OOB exception

* Renamed exception to error

* Added extend_bounds arg to load_best method

* Made pmap better cach and raise OOB exception

* Made optimizer raise error if opt params are OOB

* Linting

* Created test to ensure that OOB optimizations are caught

* Updated changelog

* Linting

* Made test be more general and robust

* Allow float-esque equality

* Changed/set extend_bounds defaults

* Use pytest.raises

c3/c3objs.py
@@ -9,6 +9,10 @@ from tensorflow.python.framework import ops
 import copy
 
 
+class QuantityOOBException(ValueError):
+    pass
+
+
 class C3obj:
     """
     Represents an abstract object with parameters. To be inherited from.
@@ -281,8 +285,16 @@ class Quantity:
             2 * (tf.reshape(val, self.shape) * self.pref - self.offset) / self.scale - 1
         )
 
-        if np.any(tf.math.abs(tmp) > tf.constant(1.0, tf.float64)):
-            raise ValueError(
+        const_1 = tf.constant(1.0, tf.float64)
+        if np.any(
+            tf.math.logical_and(
+                tf.math.abs(tmp) > const_1,
+                tf.math.logical_not(
+                    tf.experimental.numpy.isclose(tf.math.abs(tmp), const_1)
+                ),
+            )
+        ):
+            raise QuantityOOBException(
                 f"Value {num3str(val.numpy())}{self.unit} out of bounds for quantity with "
                 f"min_val: {num3str(self.get_limits()[0])}{self.unit} and "
                 f"max_val: {num3str(self.get_limits()[1])}{self.unit}",