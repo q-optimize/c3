b79f029ebefad1dcb39ec5596590846df6d854b5
Nicolas Wittler <38694261+nwittler@users.noreply.github.com>
2024-01-25 13:39:59 +0100

    Latest TF version for python 3.7

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
         "rich>=9.2.0",
         "numpy>=1.19.5",
         "scipy>=1.5.2",
-        "tensorflow==2.14.1",
+        "tensorflow==2.11.0",
         "tensorflow-estimator>=2.4.0",
         "tensorflow-probability>=0.12.1",
     ],
